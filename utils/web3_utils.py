d547c38294fe4df1004f92aa2e2bb37b9721f0ad
Christopher W <christopher.wiraatmaja95@gmail.com>
2025-07-31 20:43:52 +0900

    Finished Deploy

--- utils/web3_utils.py
+++ utils/web3_utils.py
@@ -17,10 +17,44 @@ def compile_contract(contract_name : str):
     output = compiled[f"{relative_path}:{contract_name}"]
     return output
 
-def deploy_contract(contract_name : str, web3_http_provider : Web3.HTTPProvider, compiled_output):
-    compiled_web3_contract = web3_http_provider.eth.contract(abi=compiled_output['abi'], bytecode=compiled_output['bin'])
-    # We use the default account to send the transaction (e.g., the first account in the HD wallet)
-    tx_hash = compiled_web3_contract.constructor().transact()
-    tx_receipt = web3_http_provider.eth.wait_for_transaction_receipt(tx_hash)
-    deployed_web3_contract = web3_http_provider.eth.contract(address=tx_receipt.contractAddress, abi=compiled_output['abi'])
-    return (deployed_web3_contract, tx_receipt)
+def deploy_contract(
+    web3_provider: Web3, 
+    compiled_output: dict, 
+    *constructor_args, # Collects all positional arguments
+    value: int = 0      # Optional: for payable constructors
+):
+    """
+    Deploys a contract and waits for the receipt.
+    This function is flexible and accepts any number of constructor arguments.
+
+    Args:
+        web3_provider: The connected Web3 instance.
+        compiled_output: The dictionary containing the contract's 'abi' and 'bin'.
+        *constructor_args: A variable number of arguments to pass to the contract's constructor.
+        value: The amount of ETH (in wei) to send with the deployment (for payable constructors).
+
+    Returns:
+        A tuple containing the deployed contract instance and the transaction receipt.
+    """
+    contract = web3_provider.eth.contract(
+        abi=compiled_output['abi'], 
+        bytecode=compiled_output['bin']
+    )
+    
+    # Use the first account provided by the node as the deployer
+    deployer_account = web3_provider.eth.accounts[0]
+
+    # Pass the collected constructor arguments directly to the constructor() call
+    tx_hash = contract.constructor(*constructor_args).transact({
+        'from': deployer_account,
+        'value': value
+    })
+
+    tx_receipt = web3_provider.eth.wait_for_transaction_receipt(tx_hash)
+    
+    deployed_contract = web3_provider.eth.contract(
+        address=tx_receipt.contractAddress, 
+        abi=compiled_output['abi']
+    )
+    
+    return (deployed_contract, tx_receipt)
